043ad3eb01a5e4b1dfc06c48bb0895562715df08
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-23 11:13:37 -0800

    add RecursiveBinarySearch

---
+++ src/homework1/RecursiveBinarySearch.java
@@ -0,0 +1,22 @@
+package homework1;
+
+public class RecursiveBinarySearch {
+    public static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high = list.length - 1;
+        return binarySearch(list, key, low, high);
+    }
+
+    private static int binarySearch(int[] list, int key, int low, int high) {
+        if (low > high)
+            return - low - 1;
+
+        int mid = (low + high) / 2;
+        if (key < list[mid])
+            return binarySearch(list, key, low, mid - 1);
+        else if (key == list[mid])
+            return mid;
+        else
+            return binarySearch(list, key, mid + 1, high);
+    }
+}
