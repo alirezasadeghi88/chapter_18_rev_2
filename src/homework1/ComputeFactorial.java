06af22e7808ec74c5a3c23d6576936a0e42f688a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-23 10:55:03 -0800

    F

---
+++ src/homework1/ComputeFactorial.java
@@ -0,0 +1,21 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class ComputeFactorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a nonnegative integer: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Factorial of " + n + " is " + factorial(n));
+    }
+
+    public static long factorial(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+}
